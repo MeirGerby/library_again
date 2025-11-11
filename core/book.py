7f2b79d53030d7a479bce95568c98e341188ef69
My Name <mdavidl@gmail.com>
2025-11-11 11:37:42 +0200

    book class

---
+++ core/book.py
@@ -0,0 +1,13 @@
+class Book:
+
+    def __init__(self, title: str, author: str, isbn: int):
+        self.title: str = title
+        self.author: str = author
+        self.isbn: int = isbn
+        self.is_available: bool = True
+
+    def __str__(self):
+        return f'------Book------\n Title: {self.title}\n Author: {self.author}'
+
+    def set_available(self):
+        return not self.is_available
